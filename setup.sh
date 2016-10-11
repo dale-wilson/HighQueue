9f6c19a4341105576149f48a9a56fcfc8e98fda6
Dale Wilson <wilsond@ociweb.com>
2016-10-11 15:48:12 -0500

    Linux includes

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Command file to set HighQueue environment
-# HighQueue depends on MPC V 4.1 or later. (http://www.ociweb.com/products/mpc)
-# HQTest and HQPerformance depend on BOOST Test.  Tested with 1.57.0. (http://www.boost.org/)
+# Depemds om MPC https://github.com/DOCGroup/MPC
+# HQTest and HQPerformance depend on BOOST Test 1.57.0 or later.  (http://www.boost.org/)
 # Customize this file by setting variables to suit your environment
 SOURCE="${BASH_SOURCE[0]}"
 SOURCE_DIR=`dirname $SOURCE`
@@ -8,7 +8,7 @@ export HighQueue_ROOT=`readlink -f $SOURCE_DIR`
 
 if test "$MPC_ROOT" = ""
 then
-  export MPC_ROOT=~/MPC
+  export MPC_ROOT=/MPC
 fi
 
 if test "$BOOST_ROOT" = ""
@@ -16,16 +16,23 @@ then
   export BOOST_ROOT=/usr/local
 fi
 
-if test "$BOOST_ROOT_LIB" = ""
+if test "$BOOST_LIB_PATH" = ""
 then
-  export BOOST_ROOT_LIB=$BOOST_ROOT/lib
+  export BOOST_LIB_PSTH=$BOOST_ROOT/lib
 fi
 
 if test "$BOOST_VERSION" = ""
 then
-  export BOOST_VERSION=boost-1_57
+  export BOOST_VERSION=boost-1_61
+fi
+
+if test "$KERNEL_INCLUDE" = ""
+then
+  export KERNEL_INCLUDE=/usr/src/kernels/3.10.0-327.13.1.el7.xppsl_1.3.3.151.x86_64
+  export LINUX_INCLUDE=$KERNEL_INCLUDE/include
+  export ARCH_INCLUDE=$KERNEL_INCLUDE/arch/x86/include
 fi
 
 export PATH=$HighQueue_ROOT/bin:$MPC_ROOT:$PATH
-export LD_LIBRARY_PATH=$HighQueue_ROOT/lib:$BOOST_ROOT_LIB:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HighQueue_ROOT/lib:$BOOST_LIB_PATB:$LD_LIBRARY_PATH
 
